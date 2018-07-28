2647f6b6d5b0519dffe27e3e7384e3bc5e023aaf
jojoldu <jojoldu@gmail.com>
2018-07-28 09:54:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# spring-batch-in-action
+
+Spring Batch In Action이 2011년 이후 개정판이 나오지도 않고, 한글 번역판도 없고, 국내 Spring Batch 글 대부분이 튜토리얼이거나 공식문서 번역본이라 답답해서 시작  
+커머스나 기타 서비스 시스템이 커질수록 배치 작업이 늘어나는데, 주먹구구식으로 Spring Batch 사용하는 경우가 많음  
+Spring MVC는 자료가 정말 많지만 Batch는 너무 없어서 정리도 할겸 시작.  
+
+> 잘되면 책낼수 있지 않을까?
---
+++ src/main/java/com/jojoldu/spring/springbatchinaction/SpringBatchInActionApplication.java
@@ -0,0 +1,12 @@
+package com.jojoldu.spring.springbatchinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchInActionApplication.class, args);
+    }
+}
