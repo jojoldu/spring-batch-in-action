d38f5705a4e86b98bd57781f8e611f6e8c399495
이동욱 <jojoldu@gmail.com>
2020-04-13 11:20:55 +0900

    Gradle Wrapper 버전 업

--- src/test/java/com/jojoldu/batch/multithread/MultiThreadPagingConfigurationTest.java
+++ src/test/java/com/jojoldu/batch/multithread/MultiThreadPagingConfigurationTest.java
@@ -1,19 +1,31 @@
 package com.jojoldu.batch.multithread;
 
 import com.jojoldu.batch.TestBatchConfig;
+import com.jojoldu.batch.reader.jpa.Product;
+import com.jojoldu.batch.reader.jpa.ProductBackup;
+import com.jojoldu.batch.reader.jpa.ProductBackupRepository;
+import com.jojoldu.batch.reader.jpa.ProductRepository;
+import com.jojoldu.batch.reader.jpa.ProductStatus;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 /**
  * Created by jojoldu@gmail.com on 12/04/2020
@@ -24,23 +36,53 @@ import static org.junit.Assert.assertThat;
 @ExtendWith(SpringExtension.class)
 @SpringBatchTest
 @SpringBootTest(classes={MultiThreadPagingConfiguration.class, TestBatchConfig.class})
+@TestPropertySource(properties = {"chunkSize=1", "poolSize=1"})
 public class MultiThreadPagingConfigurationTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductBackupRepository productBackupRepository;
 
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
 
-    @Test
-    public void A_실패테스트() throws Exception {
-        //given
-        String requestDate = "2020-03-09";
-        JobParametersBuilder builder = new JobParametersBuilder();
-        builder.addString("requestDate", requestDate);
+    @AfterEach
+    void after() {
+        productRepository.deleteAll();
+        productBackupRepository.deleteAll();
+    }
 
+    @Test
+    public void 페이징_분산처리_된다() throws Exception {
+        //given
+        LocalDate createDate = LocalDate.of(2020,4,13);
+        ProductStatus status = ProductStatus.APPROVE;
+        long price = 1000L;
+        for (int i = 0; i < 10; i++) {
+            productRepository.save(Product.builder()
+                    .price(i * price)
+                    .createDate(createDate)
+                    .status(status)
+                    .build());
+        }
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("createDate", createDate.toString())
+                .addString("status", status.name())
+                .toJobParameters();
         //when
-        JobExecution jobExecution = jobLauncherTestUtils.launchJob(builder.toJobParameters());
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
         //then
-        assertThat(jobExecution.getStatus(), is(BatchStatus.FAILED));
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+        List<ProductBackup> backups = productBackupRepository.findAll();
+        backups.sort(Comparator.comparingLong(ProductBackup::getPrice));
+
+        assertThat(backups).hasSize(10);
+        assertThat(backups.get(0).getPrice()).isEqualTo(0L);
+        assertThat(backups.get(9).getPrice()).isEqualTo(9000L);
     }
 
 }
