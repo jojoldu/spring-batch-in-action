f108f8c489cbc7b2ca864b74ca598502ef11e81b
이동욱 <jojoldu@gmail.com>
2020-06-19 21:18:15 +0900

    예제2 코드 완성

---
+++ src/main/java/com/jojoldu/batch/example/performancewrite/test2/StoreBackup2Configuration.java
@@ -0,0 +1,94 @@
+package com.jojoldu.batch.example.performancewrite.test2;
+
+import com.jojoldu.batch.entity.product.Store;
+import com.jojoldu.batch.entity.product.backup.StoreBackup;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+
+
+/**
+ * Created by jojoldu@gmail.com on 06/10/2019
+ * Blog : http://jojoldu.tistory.com
+ * Github : http://github.com/jojoldu
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class StoreBackup2Configuration {
+    public static final String JOB_NAME = "storeBackupJob2";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory emf;
+
+    private int chunkSize;
+
+    @Value("${chunkSize:1000}")
+    public void setChunkSize(int chunkSize) {
+        this.chunkSize = chunkSize;
+    }
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(step())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step step() {
+        return stepBuilderFactory.get("step")
+                .<Store, StoreBackup>chunk(chunkSize)
+                .reader(reader(null))
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaPagingItemReader<Store> reader(@Value("#{jobParameters[storeName]}") String storeName) {
+        String query = String.format(
+                "SELECT s " +
+                "FROM Store s " +
+                "JOIN FETCH s.products " +
+                "WHERE s.name ='%s'", storeName);
+
+        return new JpaPagingItemReaderBuilder<Store>()
+                .entityManagerFactory(emf)
+                .queryString(query)
+                .pageSize(chunkSize)
+                .name("reader")
+                .build();
+    }
+
+    private ItemProcessor<Store, StoreBackup> processor() {
+        return StoreBackup::new;
+    }
+
+    @Bean
+    public JpaItemWriter<StoreBackup> writer() {
+        return new JpaItemWriterBuilder<StoreBackup>()
+                .entityManagerFactory(emf)
+                .usePersist(true)
+                .build();
+    }
+}
